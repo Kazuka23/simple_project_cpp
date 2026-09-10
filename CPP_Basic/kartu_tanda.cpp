136a0169d814524feb4f3c016959ce27eabaf2bb
Kazuka23 <syabanmuhammad12@gmail.com>
2026-09-10 07:39:29 +0700

    tugas

---
+++ CPP_Basic/kartu_tanda.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void data(){
+    cout << "================================" << endl;
+    cout << "KARTU TANDA PENGENAL" << endl;
+    cout << "================================" << endl;
+    cout << "Nama: Muhammad Syaban Ibrahim" << endl;
+    cout << "Kode Agent: 28" << endl;
+    cout << "Status: Aktif" << endl;
+    cout << "================================" << endl;
+
+}
+
+int main(){
+    data();
+    return 0;
+
+}
